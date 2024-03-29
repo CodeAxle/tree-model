b6170101f0926bcc14d96d816ac881447326170a
CodeAxle <119520698+CodeAxle@users.noreply.github.com>
2024-03-29 22:17:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def predict():
     print(output)
     return render_template('home.html', prediction_text="Did the passenger survived : {}".format(output))
 
-if __name__=="__main__":
-    app.run(debug=True)   
+# if __name__=="__main__":
+#     app.run(debug=True)   
